47cfbbe98e8cfd22ca26ed6d94776982dfb6e32a
gabriel <gabriel.brousset01@gmail.com>
2024-12-02 03:30:20 -0500

    consistent naming and better logs

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,5 +3,5 @@ from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .gpt_greedy_corners_agent import StudentAgent
 from .agent0 import Agent0
-from .agent1 import SecondAgent
+from .agent1 import Agent1
 from .ab_agent import AB_Agent
